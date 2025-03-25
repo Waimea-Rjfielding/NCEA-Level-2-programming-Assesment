d1f7a9df64f421337ec3b42e7154542451b84f15
Waimea-Rjfielding <rjfielding@waimea.school.nz>
2025-03-25 15:03:42 +1300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -18,36 +18,62 @@ import kotlin.random.Random
 const val GAMESQUARES = 20
 const val MINCOINS = 5
 const val MAXCOINS = 15
-const val EMPTY = " "
+const val EMPTY = "BLNK"
 val COINAMMOUNT = Random.nextInt(from = MINCOINS, until = MAXCOINS)
+const val GOLDCOIN = "GOLD"
+const val SILVERCOIN = "SILV"
 fun main() {
+//make game grid using const
     val gameBoard = mutableListOf<String>()
     for (i in 1..GAMESQUARES) gameBoard.add(EMPTY)
-//make game grid using const
-    showGameBoard(gameBoard)
 
 
 //randomly put an ammount of coins on the play grid using a const for the num of silver coins
-}
+    placeCoinsInGrid(gameBoard)
+    showGameBoard(gameBoard)
+    selectCoin(gameBoard)
 
+}
+fun placeCoinsInGrid(gameBoard: MutableList<String>) {
+    repeat(COINAMMOUNT) {
+        var coinPlacement = Random.nextInt(from = 0, until = GAMESQUARES - 1)
+        gameBoard[coinPlacement] = SILVERCOIN.black().bgGrey()
+
+    }
+    gameBoard[Random.nextInt(from = 0, until = GAMESQUARES - 1)] = GOLDCOIN.black().bgYellow()
+}
 
 
 fun showGameBoard(gameBoard: MutableList<String> = mutableListOf()) {
     println()
-    for (i in 0..<gameBoard.size - 1) {
-        print("+-------")
+    for (i in 0..<gameBoard.size) {
+
+        print("+------")
+
+    }
+    println("+")
+    for (i in gameBoard.indices) {
+        print("| ${i + 1} ".padEnd(7))
+    }
+    println("|")
+
+    for (i in 0..<gameBoard.size) {
+        print("+------")
 
     }
     println('+')
-    for (i in gameBoard.indices - 1) {
-        print("| ${gameBoard[i]}".padEnd(length = 8))
+    for (i in gameBoard.indices) {
+        print("| ${gameBoard[i]} ")
     }
     println('|')
-    for (i in 0..<gameBoard.size - 1) {
-        print("+-------")
+    for (i in 0..<gameBoard.size) {
+        print("+------")
     }
     println('+')
 }
 
+fun selectCoin(gameBoard: MutableList<String> = mutableListOf()) {
+
+}
 
 
